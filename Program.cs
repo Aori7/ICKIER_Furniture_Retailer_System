442a44725b2fc08f2446ca7a7ddb8bf1f2083718
Ada <n.adawiyahisham@gmail.com>
2026-08-11 06:01:45 +0800

    Program testing.

--- Program.cs
+++ Program.cs
@@ -3,9 +3,33 @@
  * Ada, Rui Min, Zi Ying, Christina
  */
 
+//ada's program testing
+
 using ICKIER_Furniture_Retailer_System;
 //testing furniture creation for factory pattern
 FurnitureCreator creator = new TableCreator();
 
 FurnitureItem table = creator.OrderFurniture();
 
+// abstract factrory
+FurnitureFactory oakFactory =
+    new OakFurnitureFactory();
+
+Table oakTable =
+    oakFactory.CreateTable();
+
+Chair oakChair =
+    oakFactory.CreateChair();
+
+BookShelf oakShelf =
+    oakFactory.CreateBookShelf();
+
+
+Console.WriteLine(
+    oakTable.GetDescription());
+
+Console.WriteLine(
+    oakChair.GetDescription());
+
+Console.WriteLine(
+    oakShelf.GetDescription());
