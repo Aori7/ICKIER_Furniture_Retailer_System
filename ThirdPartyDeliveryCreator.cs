1ea25e0ac2571b6cdc6bc20d0d1bef9abc3a454f
Ada <n.adawiyahisham@gmail.com>
2026-08-16 05:19:42 +0800

    factory method program

---
+++ ThirdPartyDeliveryCreator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICKIER_Furniture_Retailer_System
+{
+    public class ThirdPartyDeliveryCreator : DeliveryCreator
+    {
+        public override Delivery CreateDelivery(int deliveryId, string deliveryAddress, DateTime scheduledDate, string trackingNumber)
+        {
+            return new ThirdPartyDeliveryAdapter(deliveryId, deliveryAddress, scheduledDate, trackingNumber, new ExternalCourierService());
+        }
+    }
+}
